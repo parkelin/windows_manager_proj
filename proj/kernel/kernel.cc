28e79f73a967bb5996156deccb40154a702ee8cc
Katherine Chao <kat2chao@cs.utexas.edu>
2023-04-15 00:12:34 -0500

    saving ports

--- proj/kernel/kernel.cc
+++ proj/kernel/kernel.cc
@@ -34,7 +34,46 @@ namespace gheith {
 
 
 void kernelMain(void) {
-     //change VGA mode to text mode
+    //set up port 0x3c0, write index and data to same port
+    unsigned char value;
+    Ports firstport(0x3DA);
+    unsigned char status = firstport.read();
+    if((status & 0x01) == 0x01){
+        Ports tempport(0x3C0);
+        tempport.write(0x0A);
+    }    
+    Ports secondport(0x3C1);
+    value = secondport.read();
+
+    //port 0x3c2: miscellaneous output register
+    unsigned char colorGraphicsMode = 0x01;
+    unsigned char enableVideoBit = 0x20; //must combine these
+    Ports miscPort(0x3C2);
+    //enable VGA display by setting bit 0 of 0x3c2 to 1
+    miscPort.write(colorGraphicsMode | enableVideoBit); //enable color graphics and text mode
+
+    
+    // accessing VGA port 0x3C4 
+    Ports tempport(0x3C4);
+    Ports tempporttwo(0x3C5);
+    tempport.write(0x01);
+    tempporttwo.write(0xFF);
+
+    // accessing VGA port 0x3CE
+
+    Ports tempportthree(0x3CE);
+    Ports tempportfour(0x3CF);
+    tempport.write(0x05);
+    tempporttwo.write(0xFF);
+
+
+    // accessing VGA port 0x3D4, mode control reg
+    Ports tempportfive(0x3C2); // 
+    Ports tempportsix(0x3D4);
+    Ports tempportseven(0x3D5);
+    
+
+    //change VGA mode to text mode
     Ports addressPort(0x3D4);
     addressPort.write(0x03);
     Ports dataPort(0x3D5);
