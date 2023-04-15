6ffc00b2ea921b4da0449914635baaa68c511671
Katherine Chao <kat2chao@cs.utexas.edu>
2023-04-14 22:24:37 -0500

    changed kernel.cc and init.c

--- proj/kernel/kernel.cc
+++ proj/kernel/kernel.cc
@@ -4,14 +4,55 @@
 #include "ext2.h"
 #include "sys.h"
 #include "threads.h"
+#include "ports.h"
 
 const char* initName = "/sbin/init";
+const uint16_t VGA_WIDTH = 80;
+const uint16_t VGA_HEIGHT = 25;
+uint16_t* const VGA_MEMORY = (uint16_t*)0xB8000;
 
 namespace gheith {
     Shared<Ext2> root_fs = Shared<Ext2>::make(Shared<Ide>::make(1));
 }
 
+
+// void display_string(const char* str) {
+//     uint16_t* current_pos = VGA_MEMORY;
+
+//     // Loop over each character in the string
+//     for (int i = 0; str[i] != '\0'; i++) {
+//         // If we've reached the end of a row, move to the start of the next row
+//         if ((current_pos - VGA_MEMORY) % VGA_WIDTH == 0 && current_pos != VGA_MEMORY) {
+//             current_pos += VGA_WIDTH;
+//         }
+
+//         // Write the character to the VGA buffer
+//         *current_pos++ = (uint16_t)str[i] | 0x0700;
+//     }
+// }
+
+
+
 void kernelMain(void) {
+     //change VGA mode to text mode
+    Ports addressPort(0x3D4);
+    addressPort.write(0x03);
+    Ports dataPort(0x3D5);
+    dataPort.write(0x00);
+    // MACHINE::outb(0x3D4, 0x03); //write value 3 to address port 0x3d4
+    // MACHINE::outb(0x3D5, 0x00); 
+
+    // Pointer to video memory
+    unsigned short* video_memory = (unsigned short*) 0xB8000;
+    while(1) {
+        // Output a string to the screen
+        const char* message = "Hello, world!";
+        for (int i = 0; message[i] != '\0'; ++i) {
+            video_memory[i] = (video_memory[i] & 0xFF00) | message[i];
+        }
+    }
+    
+    
     auto argv = new const char* [2];
     argv[0] = "init";
     argv[1] = nullptr;
--- proj/t0.dir/sbin/init.c
+++ proj/t0.dir/sbin/init.c
@@ -1,7 +1,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include "stdint.h"
-#include "/u/ramosden/cs439/windows_manager_proj/proj/kernel/machine.h"
+// #include "ports.h"
+// #include "kernel/machine.h"
 
 // #include "/u/ramosden/cs439/windows_manager_proj/windows_manager_proj/kernel/machine.h"
 // #include "/u/ramosden/cs439/windows_manager_proj/windows_manager_proj/kernel/ports.h"
@@ -21,30 +22,36 @@
 
 // }
 
-void display_words(const char* str, unsigned char color) {
-    unsigned short * vga_memory_addr = (unsigned short*)0xB8000;
+// void display_words(const char* str, unsigned char color) {
+//     unsigned short * vga_memory_addr = (unsigned short*)0xB8000;
 
-    while(*str){
-        unsigned short data = (unsigned char)(*str++) | ((unsigned short) color << 8);
-        *vga_memory_addr++ = data;
-    }
-}
+//     while(*str){
+//         unsigned short data = (unsigned char)(*str++) | ((unsigned short) color << 8);
+//         *vga_memory_addr++ = data;
+//     }
+// }
 
 int main() {
     //change VGA mode to text mode
-    // Ports * addressPort(0x3D4);
+    // Ports addressPort(0x3D4);
     // Ports dataPort(0x3D5);
     
-    // addressPort.write(0x03);
-    // dataPort.write(0x00);
-    MACHINE::outb(0x3D4, 0x03); //write value 3 to address port 0x3d4
-    MACHINE::outb(0x3D5, 0x00); 
+    // // addressPort.write(0x03);
+    // // dataPort.write(0x00);
+    // MACHINE::outb(0x3D4, 0x03); //write value 3 to address port 0x3d4
+    // MACHINE::outb(0x3D5, 0x00); 
 
 
-
-    // const char* str = "Hi from Denise and Kat!";
-    // unsigned char color = 0xF;
-    // display_words(str, color);
+    // Pointer to video memory
+    unsigned short* video_memory = (unsigned short*) 0xB8000;
+    while(1) {
+        // Output a string to the screen
+        const char* message = "Hello, world!";
+        for (int i = 0; message[i] != '\0'; ++i) {
+            video_memory[i] = (video_memory[i] & 0xFF00) | message[i];
+        }
+    }
+    
 
 
     // while (true) {
